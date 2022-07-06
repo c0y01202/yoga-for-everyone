2dec543b20aa2a0ca4c07534693ca8ff52c678a2
VeroKoles <97703518+verokoles@users.noreply.github.com>
2022-07-05 19:38:26 -0500

    heroku created

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
 mongoose.connect(
-  process.env.MONGDB_URI || "mongodb://localhost:27017/book-search",
+  process.env.MONGDB_URI || "mongodb://localhost:27017/yoga-for-everyone",
   {
     useNewUrlParser: true,
     useUnifiedTopology: true,
