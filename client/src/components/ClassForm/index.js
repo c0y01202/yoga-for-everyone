c10ab66a707b9c240caf26f9d7cd327ed83e9f13
Colt <98503459+coltschultz@users.noreply.github.com>
2022-07-11 10:46:44 -0500

    touch up

--- client/src/components/ClassForm/index.js
+++ client/src/components/ClassForm/index.js
@@ -65,9 +65,9 @@ const ClassForm = () => {
           <Form.Label htmlFor='classText'>Class:</Form.Label>
           <Form.Select className="form-input col-9 col-md-9" name="classText" onChange={handleInputChange} value={classFormData.classText} aria-label="Default select example">
             <option>Select A Class</option>
-            <option value="Aerial-With-Claudia">Aerial Yoga With Claudia</option>
-            <option value="Ashtanga-With-John">Ashtanga with John</option>
-            <option value="Vinyasa-With-Veronica">Vinyasa with Veronica</option>
+            <option value="Aerial with Claudia">Aerial Yoga With Claudia</option>
+            <option value="Ashtanga with John">Ashtanga with John</option>
+            <option value="Vinyasa with Veronica">Vinyasa with Veronica</option>
           </Form.Select>
         </Form.Group>
 
@@ -75,10 +75,10 @@ const ClassForm = () => {
           <Form.Label htmlFor='testText'>Time:</Form.Label>
           <Form.Select className="form-input col-9 col-md-9" name="testText" onChange={handleInputChange} value={classFormData.testText} aria-label="Default select example">
             <option>Select A Time</option>
-            <option value="10am-Tue-Thu">10am - Tue/Thu</option>
-            <option value="10am-Mon-Wed">10am - Mon/Wed</option>
-            <option value="6pm-Tue-Thu">6pm - Tue/Thu</option>
-            <option value="6pm-Mon-Wed">6pm - Mon/Wed</option>
+            <option value="10am Tue/Thu">10am - Tue/Thu</option>
+            <option value="10am Mon/Wed">10am - Mon/Wed</option>
+            <option value="6pm Tue/Thu">6pm - Tue/Thu</option>
+            <option value="6pm Mon/Wed">6pm - Mon/Wed</option>
           </Form.Select>
         </Form.Group>
         <Button
