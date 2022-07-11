431b87d1b4925798576705a838858400d2a73437
John-Pena <jmp1495@gmail.com>
2022-07-10 20:51:34 -0500

    Virtual class style set up

--- client/src/pages/VirtualClasses.js
+++ client/src/pages/VirtualClasses.js
@@ -1,4 +1,5 @@
 import React from 'react';
+
 import virtualOne from '../images/virtual_classes_page/vistural_class_1.png';
 import virtualTwo from '../images/virtual_classes_page/vistural_class_2.png';
 import virtualThree from '../images/virtual_classes_page/vistural_class_3.png';
@@ -6,10 +7,16 @@ import virtualThree from '../images/virtual_classes_page/vistural_class_3.png';
 
 const VirtualClass = () => {
   return(
-    <div>
-      <img src={virtualOne} alt='online course option 1' height={200} width={200} />
-      <img src={virtualTwo} alt='online course option 2' height={200} width={200} />
-      <img src={virtualThree} alt='online course option 3' height={200} width={200} />
+    <div className='flex-row'>
+      <div className='col-sm-12 col-md-4'>
+        <img src={virtualOne} alt='online course option 1' height={300} width={300} />
+      </div>
+      <div className='col-sm-12 col-md-4'>
+        <img src={virtualTwo} alt='online course option 2' height={300} width={300} />
+      </div>
+      <div className='col-sm-12 col-md-4'>
+        <img src={virtualThree} alt='online course option 3' height={300} width={300} />
+      </div>
     </div>
   );
 };
