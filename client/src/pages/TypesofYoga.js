f51d8e50813b76c51cde8b682b6a913bcd3db9c8
John-Pena <jmp1495@gmail.com>
2022-07-11 19:01:37 -0500

    images rendered on types of yoga page

--- client/src/pages/TypesofYoga.js
+++ client/src/pages/TypesofYoga.js
@@ -1,18 +1,56 @@
 import React from "react";
 
 import aerial from "../components/react_images/TypesofYoga/aerial.png";
+import ashtanga from "../components/react_images/TypesofYoga/ashtanga.png";
+import hatha from "../components/react_images/TypesofYoga/hatha.png";
+import kundalini from "../components/react_images/TypesofYoga/kundalini.png";
+import yin from "../components/react_images/TypesofYoga/yin.png";
 
 import "../App.css";
 
 function TypesofYoga() {
   return (
     <div className="TypesofYoga">
-      <img
-        src={aerial}
-        alt="Aerial Yoga"
-        height={200}
-        width={300}
-      />
+      <div>
+        <img
+          src={aerial}
+          alt="Aerial Yoga"
+          height={200}
+          width={300}
+        />
+      </div>
+      <div>
+        <img
+          src={ashtanga}
+          alt="Ashtanga Yoga"
+          height={200}
+          width={300}
+        />
+      </div>
+      <div>
+        <img
+          src={hatha}
+          alt="Hatha Yoga"
+          height={200}
+          width={300}
+        />
+      </div>
+      <div>
+        <img
+          src={kundalini}
+          alt="Kundalini Yoga"
+          height={200}
+          width={300}
+        />
+      </div>
+      <div>
+        <img
+          src={yin}
+          alt="Yin Yoga"
+          height={200}
+          width={300}
+        />
+      </div>
     </div>
   );
 }
