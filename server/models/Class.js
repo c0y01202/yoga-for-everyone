7c6aaba6adb7426945170f377b18a5e5529e1e24
Claudia Chittim <cchittim10101@gmail.com>
2022-07-12 06:39:09 -0500

    reinstated line 42 on class.js

--- server/models/Class.js
+++ server/models/Class.js
@@ -1,44 +1,41 @@
-const { Schema, model, models } = require("mongoose");
-const reactionSchema = require("./Reaction");
-const dateFormat = require("../utils/dateFormat");
+const { Schema, model } = require('mongoose');
+const reactionSchema = require('./Reaction');
+const dateFormat = require('../utils/dateFormat');
 
 const classSchema = new Schema(
   {
     classText: {
       type: String,
       minlength: 1,
-      maxlength: 280,
+      maxlength: 280
     },
     testText: {
       type: String,
       minlength: 1,
-      maxlength: 280,
+      maxlength: 280
     },
     createdAt: {
       type: Date,
       default: Date.now,
-      get: (timestamp) => dateFormat(timestamp),
+      get: timestamp => dateFormat(timestamp)
     },
     username: {
       type: String,
-      required: true,
+      required: true
     },
-    reactions: [reactionSchema],
+    reactions: [reactionSchema]
   },
   {
     toJSON: {
-      getters: true,
-    },
+      getters: true
+    }
   }
 );
 
-classSchema.virtual("reactionCount").get(function () {
+classSchema.virtual('reactionCount').get(function() {
   return this.reactions.length;
 });
 
-const Class = model("Class", classSchema);
+const Class = model('Class', classSchema);
 
-module.exports = {
-  Class,
-  locationSchema,
-};
+module.exports = Class;
