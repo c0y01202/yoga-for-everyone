fe02c9cf5c3a5e7481d481a753e387fa9aea8c61
Claudia Chittim <cchittim10101@gmail.com>
2022-07-11 21:32:34 -0500

    changed code in connection.js

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
 mongoose.connect(
-  process.env.MONGDB_URI || "mongodb://localhost:27017/yoga-for-everyone",
+  process.env.MONGODB_URI || "mongodb://localhost:27017/yoga-for-everyone",
   {
     useNewUrlParser: true,
     useUnifiedTopology: true,
