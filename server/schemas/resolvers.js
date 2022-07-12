75a0d4aa80b511756113e936e880ff0221bfd1ab
Claudia Chittim <cchittim10101@gmail.com>
2022-07-12 06:21:30 -0500

    changed code in class.js line 42-44

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,32 +1,32 @@
-const { AuthenticationError } = require('apollo-server-express');
-const { User, Class } = require('../models');
-const { signToken } = require('../utils/auth');
+const { AuthenticationError } = require("apollo-server-express");
+const { User, Class } = require("../models");
+const { signToken } = require("../utils/auth");
 
 const resolvers = {
   Query: {
     me: async (parent, args, context) => {
       if (context.user) {
         const userData = await User.findOne({ _id: context.user._id })
-          .select('-__v -password')
-          .populate('classes')
-          .populate('friends');
+          .select("-__v -password")
+          .populate("classes")
+          .populate("friends");
 
         return userData;
       }
 
-      throw new AuthenticationError('Not logged in');
+      throw new AuthenticationError("Not logged in");
     },
     users: async () => {
       return User.find()
-        .select('-__v -password')
-        .populate('classes')
-        .populate('friends');
+        .select("-__v -password")
+        .populate("classes")
+        .populate("friends");
     },
     user: async (parent, { username }) => {
       return User.findOne({ username })
-        .select('-__v -password')
-        .populate('friends')
-        .populate('classes');
+        .select("-__v -password")
+        .populate("friends")
+        .populate("classes");
     },
     classes: async (parent, { username }) => {
       const params = username ? { username } : {};
@@ -34,7 +34,7 @@ const resolvers = {
     },
     class: async (parent, { _id }) => {
       return Class.findOne({ _id });
-    }
+    },
   },
 
   Mutation: {
@@ -48,13 +48,13 @@ const resolvers = {
       const user = await User.findOne({ email });
 
       if (!user) {
-        throw new AuthenticationError('Incorrect credentials');
+        throw new AuthenticationError("Incorrect credentials");
       }
 
       const correctPw = await user.isCorrectPassword(password);
 
       if (!correctPw) {
-        throw new AuthenticationError('Incorrect credentials');
+        throw new AuthenticationError("Incorrect credentials");
       }
 
       const token = signToken(user);
@@ -62,8 +62,11 @@ const resolvers = {
     },
     addClass: async (parent, args, context) => {
       if (context.user) {
-        const class1 = await Class.create({ ...args, username: context.user.username });
-        
+        const class1 = await Class.create({
+          ...args,
+          username: context.user.username,
+        });
+
         await User.findByIdAndUpdate(
           { _id: context.user._id },
           { $push: { classes: class1._id } },
@@ -73,20 +76,24 @@ const resolvers = {
         return class1;
       }
 
-      throw new AuthenticationError('You need to be logged in!');
+      throw new AuthenticationError("You need to be logged in!");
     },
     addReaction: async (parent, { classId, reactionBody }, context) => {
       if (context.user) {
         const updatedClass = await Class.findOneAndUpdate(
           { _id: classId },
-          { $push: { reactions: { reactionBody, username: context.user.username } } },
+          {
+            $push: {
+              reactions: { reactionBody, username: context.user.username },
+            },
+          },
           { new: true, runValidators: true }
         );
 
         return updatedClass;
       }
 
-      throw new AuthenticationError('You need to be logged in!');
+      throw new AuthenticationError("You need to be logged in!");
     },
     addFriend: async (parent, { friendId }, context) => {
       if (context.user) {
@@ -94,14 +101,14 @@ const resolvers = {
           { _id: context.user._id },
           { $addToSet: { friends: friendId } },
           { new: true }
-        ).populate('friends');
+        ).populate("friends");
 
         return updatedUser;
       }
 
-      throw new AuthenticationError('You need to be logged in!');
-    }
-  }
+      throw new AuthenticationError("You need to be logged in!");
+    },
+  },
 };
 
 module.exports = resolvers;
